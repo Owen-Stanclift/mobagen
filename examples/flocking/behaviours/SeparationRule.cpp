397dd551e8fc4f8e8926318331e776f94e3e169e
Owen <owen.stanclift@mymail.champlain.edu>
2024-09-10 14:21:05 -0400

    Game of Life

--- examples/flocking/behaviours/SeparationRule.cpp
+++ examples/flocking/behaviours/SeparationRule.cpp
@@ -4,44 +4,20 @@
 #include "engine/Engine.h"
 
 Vector2f SeparationRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
-  // Try to avoid boids too close
-
-
   Vector2f separatingForce = Vector2f::zero();
-      float desiredDistance = desiredMinimalDistance;
+  float desiredDistance = desiredMinimalDistance;
   //    // todo: implement a force that if neighbor(s) enter the radius, moves the boid away from it/them
-      for (auto n : neighborhood)
-        {
-        Vector2f dir = n->transform.position - boid->transform.position;
-        float dist = dir.getMagnitude();
-        if(dist <= desiredDistance && dist > 0) {
-            Vector2f hat = dir.normalized();
-            separatingForce = hat/dist;
-          if(separatingForce.getMagnitude() > maxForce) //Max force is 2 as no clue if it was given
-            {
-            Vector2f forceHat = separatingForce.normalized();
-            return forceHat * maxForce;
-          }
-          else {
-            return separatingForce;
-          }
-        }
-        else {
-          return Vector2f::zero();
-        }
-      }
+  for (auto n : neighborhood)
+  {
+    Vector2f dir =  boid->transform.position - n->transform.position;
+    float dist = dir.getMagnitude();
+    if(dist <= desiredDistance && dist > 0) {
+      Vector2f hat = dir.normalized();
+      separatingForce += hat/dist;
+    }
+  }
 
-     if (!neighborhood.empty()) {
-         Vector2f position = boid->transform.position;
-         int countCloseFlockmates = 0;
-  //        // todo: find and apply force only on the closest mates
-       separatingForce = (separatingForce);
-
-      }
-
-
-
-  return separatingForce;
+  return separatingForce*desiredDistance;
 }
 
 bool SeparationRule::drawImguiRuleExtra() {
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -3,9 +3,22 @@
 // Reference: https://playgameoflife.com/info
 void JohnConway::Step(World& world) {
   // todo: implement
+  for(int i = 0; i < world.SideSize(); i++) {
+    for(int j = 0; j < world.SideSize(); j++) {
+
+    }
+  }
+  //world.SetNext()
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
-  return 0;
+  int acc = 0;
+  for(int i = -1; i <1; i++) {
+    for(int j = -1; j <1; j++) {
+      Point2D point = point + Point2D(i, j);
+      acc += world.Get(point);
+    }
+    return acc;
+  }
 }

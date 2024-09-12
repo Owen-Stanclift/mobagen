199395ed68a804459b7e63e9aac3f27f7bd526a6
Owen Stanclift <owen.stanclift@mymail.champlain.edu>
2024-09-12 09:55:34 -0400

    Finished Game of Life

--- examples/life/rules/HexagonGameOfLife.cpp
+++ examples/life/rules/HexagonGameOfLife.cpp
@@ -3,5 +3,54 @@
 //
 
 #include "HexagonGameOfLife.h"
-void HexagonGameOfLife::Step(World& world) {}
-int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) { return 0; }
+void HexagonGameOfLife::Step(World& world) {
+  for(int i = 0; i < world.SideSize(); i++) {
+    for(int j = 0; j < world.SideSize(); j++) {
+      Point2D point = Point2D(i, j);
+
+      if(world.Get(point))
+      {
+        if(CountNeighbors(world, point) < 2)
+          world.SetNext(point,false);
+        if(CountNeighbors(world, point) >= 2 && CountNeighbors(world, point) < 4)
+          world.SetNext(point,true);
+        if(CountNeighbors(world, point) >3)
+          world.SetNext(point,false);
+      }
+      else
+        if(CountNeighbors(world,point) == 3)
+          world.SetNext(point,true);
+    }
+  }
+}
+int HexagonGameOfLife::CountNeighbors(World& world, Point2D point) {
+  int acc = 0;
+  for(int j = -1; j < 2; j++) {
+    if(j == 0) {
+      Point2D checkPoint = Point2D(point.x-1, point.y);
+      if(point.x -1 >= 0)
+      acc += world.Get(checkPoint);
+      checkPoint = Point2D(point.x+1, point.y);
+      if(point.x +1 < world.SideSize())
+      acc += world.Get(checkPoint);
+    }
+    else if(point.y +j%2 == 0) {
+      Point2D checkPoint = Point2D(point.x,  point.y+j);
+      if(point.y +j < world.SideSize() && point.y+j >= 0)
+      acc += world.Get(checkPoint);
+      checkPoint = Point2D(point.x+1,  point.y+j);
+      if(point.x +1 < world.SideSize() && point.y +j < world.SideSize() && point.y+j >=0)
+      acc += world.Get(checkPoint);
+    }
+    else {
+      Point2D checkPoint = Point2D(point.x-1,  point.y+j);
+      if(point.x -1 >= 0 && point.y +j < world.SideSize() && point.y+j >=0)
+      acc += world.Get(checkPoint);
+      checkPoint = Point2D(point.x,  point.y+j);
+      if(point.y +j < world.SideSize() && point.y+j >= 0)
+      acc += world.Get(checkPoint);
+    }
+  }
+
+  return acc;
+}
--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -5,7 +5,20 @@ void JohnConway::Step(World& world) {
   // todo: implement
   for(int i = 0; i < world.SideSize(); i++) {
     for(int j = 0; j < world.SideSize(); j++) {
+      Point2D point =  Point2D(i, j);
 
+      if(world.Get(point))
+        {
+        if(CountNeighbors(world, point) < 2)
+          world.SetNext(point,false);
+        if(CountNeighbors(world, point) >= 2 && CountNeighbors(world, point) < 4)
+          world.SetNext(point,true);
+        if(CountNeighbors(world, point) >3)
+          world.SetNext(point,false);
+      }
+      else
+        if(CountNeighbors(world,point) == 3)
+          world.SetNext(point,true);
     }
   }
   //world.SetNext()
@@ -14,11 +27,14 @@ void JohnConway::Step(World& world) {
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
   int acc = 0;
-  for(int i = -1; i <1; i++) {
-    for(int j = -1; j <1; j++) {
-      Point2D point = point + Point2D(i, j);
-      acc += world.Get(point);
+  for(int i = -1; i <2; i++)
+    {
+    for(int j = -1; j <2; j++)
+      {
+      Point2D checkPoint = Point2D(point.x+i, point.y+j);
+      if(point.x+i >= 0 && point.x+i < world.SideSize()-1 && point.y+j >= 0 && point.y+j < world.SideSize())
+      acc += world.Get(checkPoint);
     }
-    return acc;
   }
+  return acc;
 }
